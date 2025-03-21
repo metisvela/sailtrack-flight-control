26cc3aa5bef86dce82865e4d2e4492d9a95ec364
Magform <nicolas25.02.2003.nf@gmail.com>
2025-03-21 10:20:21 +0100

    add option to control vertical length

--- include/MotorController.h
+++ include/MotorController.h
@@ -6,9 +6,10 @@
 
 class MotorController {
 public:
-    MotorController(uint8_t servoPin, uint16_t minAngle = 0, uint16_t maxAngle = 180);
+    MotorController(uint8_t servoPin, uint16_t armLength, uint16_t minAngle = 0, uint16_t maxAngle = 180);
     bool begin(); // Returns success status
     void setAngle(uint16_t angle);
+    void setVerticalLength(uint16_t length);
     uint16_t getCurrentAngle() const { return _currentAngle; }
 
 private:
@@ -16,6 +17,7 @@ private:
     const uint8_t _servoPin;
     const uint16_t _minAngle;
     const uint16_t _maxAngle;
+    const uint16_t _armLength;
     uint16_t _currentAngle = 0;
     bool _attached = false;
 };
--- src/MotorController.cpp
+++ src/MotorController.cpp
@@ -1,7 +1,7 @@
 #include "MotorController.h"
 
-MotorController::MotorController(uint8_t servoPin, uint16_t minAngle, uint16_t maxAngle)
-    : _servoPin(servoPin), _minAngle(minAngle), _maxAngle(maxAngle) {}
+MotorController::MotorController(uint8_t servoPin, uint16_t armLength, uint16_t minAngle, uint16_t maxAngle)
+    : _servoPin(servoPin), _minAngle(minAngle), _maxAngle(maxAngle), _armLength(armLength) {}
 
 bool MotorController::begin() {
     if (_servo.attach(_servoPin) == -1) {
@@ -18,4 +18,13 @@ void MotorController::setAngle(uint16_t angle) {
     
     _currentAngle = constrain(angle, _minAngle, _maxAngle);
     _servo.write(static_cast<int>(_currentAngle));
+}
+
+void MotorController::setVerticalLength(uint16_t length) {
+    if (!_attached) return;
+
+    float angleRadians = asin(static_cast<float>(length) / _armLength);
+    uint16_t angleDegrees = static_cast<uint16_t>(angleRadians * 180.0 / M_PI);
+
+    setAngle(angleDegrees);
 }
